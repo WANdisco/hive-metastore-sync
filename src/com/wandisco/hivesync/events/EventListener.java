1f30142ef6cbb979f1103e35259ebdcec551a516
ssw <ssw@HAWK>
2016-03-03 16:04:19 +0300

    initial commit

--- src/com/wandisco/hivesync/events/EventListener.java
+++ src/com/wandisco/hivesync/events/EventListener.java
@@ -1,13 +1,14 @@
 package com.wandisco.hivesync.events;
 
 import java.sql.Connection;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
+//import org.apache.hadoop.conf.Configuration;
+//import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.metastore.MetaStoreEventListener;
-import org.apache.hadoop.hive.metastore.api.FieldSchema;
 import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.metastore.api.Partition;
-import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.hive.metastore.events.AddPartitionEvent;
 import org.apache.hadoop.hive.metastore.events.AlterPartitionEvent;
 import org.apache.hadoop.hive.metastore.events.AlterTableEvent;
@@ -19,57 +20,89 @@ import org.apache.hadoop.hive.metastore.events.DropTableEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.wandisco.hivesync.common.Tools;
-import com.wandisco.hivesync.hive.Commands;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IQueue;
 
 public class EventListener extends MetaStoreEventListener {
   Connection outgoing = null;
 
-  boolean isMonitoring = false;
-
   private static final Logger LOG = LogManager.getLogger(EventListener.class);
 
-  private static final String CONNECTION_STRING = "metastore.replicator.connection";
+  private static final String REPL_SERVER = "metastore.replicator";
+
+  private static final String CONNECTION = "metastore.replicator.connection";
 
   private static final String USER = "metastore.replicator.user";
 
   private static final String PASS = "metastore.replicator.password";
 
+  private static IQueue<ReplicationEvent> processingQueue = null;
+  private static Map<String, String> configuration = null;
+
   public EventListener(Configuration config) {
     super(config);
     try {
-      outgoing = Tools.createNewConnection(config.get(CONNECTION_STRING), config.get(USER), config.get(PASS));
+      Config cfg = new Config();
+      HazelcastInstance hc = Hazelcast.newHazelcastInstance(cfg);
+      processingQueue = hc.getQueue("PROCESSING_QUEUE");
+
+      configuration = hc.getMap("CONFIG");
+      configuration.put("CONNECTION", config.get(CONNECTION));
+      configuration.put("USER", config.get(USER));
+      configuration.put("PASS", config.get(PASS));
     } catch (Exception e) {
-      isMonitoring = true;
-      LOG.error("Connection string : " + config.get(CONNECTION_STRING));
-      LOG.error("User : " + config.get(USER));
-      LOG.error("Password : " + config.get(PASS));
-      LOG.error("Cannot create outgoing connection. Only monitoring mode active!");
+      LOG.error("Cannot connect to replication server!");
     }
   }
 
   @Override
   public void onAddPartition(AddPartitionEvent partitionEvent) throws MetaException {
+    LOG.trace("Add partition event for : " + partitionEvent.getTable().getTableName());
+
+    try {
+      AddPartEvent ape = new AddPartEvent(partitionEvent.getTable(), partitionEvent.getPartitions());
+
+      processingQueue.add(ape);
+    } catch (Exception e) {
+      LOG.error("Cannot add partition on remote side.", e);
+    }
 
   }
 
   @Override
   public void onAlterPartition(AlterPartitionEvent partitionEvent) throws MetaException {
+    LOG.trace("Alter partition event for : " + partitionEvent.getOldPartition().getTableName());
 
+    try {
+      AlterPartEvent ape = new AlterPartEvent(partitionEvent.getNewPartition(), partitionEvent.getOldPartition());
+      processingQueue.add(ape);
+    } catch (Exception e) {
+      LOG.error("Cannot alter partition table on remote side.", e);
+    }
   }
 
   @Override
   public void onAlterTable(AlterTableEvent tableEvent) throws MetaException {
+    LOG.trace("Alter table event for : " + tableEvent.getNewTable().getTableName());
+
+    try {
+      AlterTBEvent cde = new AlterTBEvent(tableEvent.getNewTable(), tableEvent.getOldTable());
+      processingQueue.add(cde);
+    } catch (Exception e) {
+      LOG.error("Cannot alter table on remote side.", e);
+    }
 
   }
 
   @Override
   public void onCreateDatabase(CreateDatabaseEvent dbEvent) throws MetaException {
     LOG.trace("Create database event for : " + dbEvent.getDatabase().getName());
-    if (isMonitoring)
-      return;
+
     try {
-      Commands.createDatabase(outgoing, dbEvent.getDatabase().getName());
+      CreateDBEvent cde = new CreateDBEvent(dbEvent.getDatabase());
+      processingQueue.add(cde);
     } catch (Exception e) {
       LOG.error("Cannot create database on remote side.", e);
     }
@@ -77,25 +110,11 @@ public class EventListener extends MetaStoreEventListener {
 
   @Override
   public void onCreateTable(CreateTableEvent tableEvent) throws MetaException {
-    Table table = tableEvent.getTable();
-    String tableName = table.getTableName();
-    LOG.error("Create table event for : " + tableName);
-    if (isMonitoring)
-      return;
+    LOG.error("Create table event for : " + tableEvent.getTable().getTableName());
+
     try {
-      StringBuilder createCommand = new StringBuilder();
-      StringBuilder fieldList = new StringBuilder();
-      StringBuilder partList = new StringBuilder();
-      for (FieldSchema f : table.getPartitionKeys())
-        partList.append(f.getName()).append(' ');
-
-      for (FieldSchema f : table.getSd().getCols()) {
-        fieldList.append(f.getName()).append(' ').append(f.getType()).append(',');
-      }
-      fieldList.deleteCharAt(fieldList.length() - 1);
-
-      createCommand.append("CREATE TABLE ").append(tableName).append('(').append(fieldList).append(')').append("PARTITIONED BY(").append(partList).append(')');
-      Commands.executeQuery(outgoing, createCommand.toString());
+      CreateTBEvent cte = new CreateTBEvent(tableEvent.getTable());
+      processingQueue.add(cte);
     } catch (Exception e) {
       LOG.error("Cannot create table on remote side.", e);
     }
@@ -104,10 +123,10 @@ public class EventListener extends MetaStoreEventListener {
   @Override
   public void onDropDatabase(DropDatabaseEvent dbEvent) throws MetaException {
     LOG.trace("Drop database event for : " + dbEvent.getDatabase().getName());
-    if (isMonitoring)
-      return;
+
     try {
-      Commands.dropDatabase(outgoing, dbEvent.getDatabase().getName());
+      DropDBEvent dde = new DropDBEvent(dbEvent.getDatabase());
+      processingQueue.add(dde);
     } catch (Exception e) {
       LOG.error("Cannot drop database on remote side.", e);
     }
@@ -117,11 +136,10 @@ public class EventListener extends MetaStoreEventListener {
   public void onDropPartition(DropPartitionEvent partitionEvent) throws MetaException {
     Partition partition = partitionEvent.getPartition();
     LOG.trace("Drop partition event for : " + partition.getTableName());
-    if (isMonitoring)
-      return;
+
     try {
-      Table table = partitionEvent.getTable();
-      Commands.dropPartition(outgoing, table.getTableName(), Tools.buildPartitionSpec(partition, table));
+      DropPartEvent dte = new DropPartEvent(partitionEvent.getTable(), partitionEvent.getPartition(), partitionEvent.getDeleteData());
+      processingQueue.add(dte);
     } catch (Exception e) {
       LOG.error("Cannot drop partition on remote side.", e);
     }
@@ -130,10 +148,10 @@ public class EventListener extends MetaStoreEventListener {
   @Override
   public void onDropTable(DropTableEvent tableEvent) throws MetaException {
     LOG.trace("Drop table event for : " + tableEvent.getTable().getTableName());
-    if (isMonitoring)
-      return;
+
     try {
-      Commands.dropTable(outgoing, tableEvent.getTable().getTableName());
+      DropTBEvent dte = new DropTBEvent(tableEvent.getTable(), tableEvent.getDeleteData());
+      processingQueue.add(dte);
     } catch (Exception e) {
       LOG.error("Cannot drop table on remote side.", e);
     }
